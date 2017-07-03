f0f850d675661acff0f73db512f2659f0e9a65e9
= <=>
2017-07-04 01:20:47 +0300

    added a report mtd to display info about the class and improved the constructor

--- HourlyEmp.py
+++ HourlyEmp.py
@@ -1,11 +1,14 @@
 from Employee import Employee
 class Hourly_Employee(Employee):
 	def __init__(self,first_name,last_name,social_security_number, wage, hours):
-		super(Employee, self).__init__()
+		# super(Hourly_Employee, self).__init__()
 		if wage <= 0:
 			raise ValueError("Wage must be greater than zero")
 		if((hours < 0.0) or (hours > 168.0)):
 			raise ValueError("Hours worked must be between 0 and 168")
+		self._first_name = first_name
+		self._last_name = last_name
+		self._social_security_number = social_security_number
 		self._wage = wage
 		self._hours = hours
 		
@@ -26,11 +29,10 @@ class Hourly_Employee(Employee):
 		return self._hours
 
 	def earnings(self):
-		if get_hours_worked()< 40:
-			return get_hours_worked() * get_wage()
+		if self._hours< 40:
+			return self._hours * self._wage
 		#employess that work over time get a bonus of 3.5 per hour
-		return (get_hours_worked() * get_wage()) + ((get_hours_worked()-40) * 3.5)
-
-
-
-		
+		return (self._hours * self._wage) + ((self._hours-40) * 3.5)
+	def report(self):
+		earns = Hourly_Employee.earnings(self)
+		print("First Name: %s\nLast Name: %s\nSocial Number: %s\nWage: %f\nHours Worked: %f\nEarns: %f\n"%(self._first_name,self._last_name,self._social_security_number,self._wage,self._hours,earns))
