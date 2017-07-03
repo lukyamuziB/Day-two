350d9ac0b72b401ff91d03370e6eb9336d6e6981
= <=>
2017-07-04 01:21:38 +0300

    added a report mtd to display info about the class and improved the constructor

--- SalaryEmp.py
+++ SalaryEmp.py
@@ -1,10 +1,14 @@
 from Employee import Employee
 class Salaried_Employee(Employee):
-	"""docstring for Salaried_Employee"""
-	def __init__(self,first_name,last_name,social_security_number, weekly_salary):
-		super(Employee, self).__init__()
+	"""Class for an Employee that earns a weeklky salary"""
+	def __init__(self,first_name,last_name,social_security_number,weekly_salary):
+		
+
 		if weekly_salary < 0.0:
 			raise ValueError("Sorry, weekly salary cant be less than 0")
+		self._first_name = first_name
+		self._last_name = last_name
+		self._social_security_number = social_security_number
 		self._weekly_salary = weekly_salary
 
 	def set_salary(self, ammount):
@@ -15,4 +19,7 @@ class Salaried_Employee(Employee):
 
 	def earnings(self):
 		return self._weekly_salary*4
+	def report(self):
+		a = Salaried_Employee.earnings(self)
+		print("First Name: %s\nLast Name: %s\nSocial Number: %s\nWeekly Salary: %f\nEarns: %f\n"%(self._first_name,self._last_name,self._social_security_number,self._weekly_salary,a))
 
