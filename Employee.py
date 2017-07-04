eee2f25508d28c95dfbf8d65dd95b235d8fa8141
= <=>
2017-07-04 16:02:57 +0300

    removed redundant imports

--- Employee.py
+++ Employee.py
@@ -1,12 +1,10 @@
-from abc import ABC, abstractmethod,ABCMeta
+from abc import abstractmethod,ABCMeta
 class Employee(metaclass = ABCMeta):
 	"""docstring for Employee"""
 	def __init__(self, first_name, last_name, social_security_number):
 		self._first_name = first_name
 		self._last_name = last_name
 		self._social_security_number = social_security_number
-		
-
 	def get_first_name(self):
 		return self._first_name
 	def get_last_name(self):
