b204438cda50e44be59231dfa23ac560edbf47a0
= <=>
2017-07-04 16:06:15 +0300

    imprpved output

--- main.py
+++ main.py
@@ -6,24 +6,23 @@
 
 from SalaryEmp import Salaried_Employee
 from CommissionedEmp import Commissioned_Employee
-from Employee import Employee
 from HourlyEmp import Hourly_Employee
 
 try:
 
-	emp1 = Salaried_Employee("lukya","bahb","busBD",90000)
+	emp1 = Salaried_Employee("lukya", "bahb", "busBD", 90000)
 	emp2 = Commissioned_Employee("nak","kak","908-984-565",12.09,78.1)
-	emp3 = Hourly_Employee("Alexis","Sanchez","103-908-675",7000,140)
+	emp3 = Hourly_Employee("Alexis", "Sanchez", "103-908-675", 7000, 140)
 except ValueError as e:
 	print("Adjust your working values to get everything right")
 
-Employees = []
-Employees.append(emp1)
-Employees.append(emp2)
-Employees.append(emp3)
+employees = []
+employees.append(emp1)
+employees.append(emp2)
+employees.append(emp3)
 
-for emp in Employees:
+for emp in employees:
 	emp.report()
 	if  isinstance(emp, Commissioned_Employee):
 		emp.set_rate(20.78)
-		print("New earnings of the Commissioned Employed are: %f"%emp.earnings())
+		print("New earnings of the Commissioned Employed are: %.3f"%emp.earnings())
